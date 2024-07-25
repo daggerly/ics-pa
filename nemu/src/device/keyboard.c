9d334f4ddcdbf1f6e1c413ead939f93e3196c6d3
liuyang <liuyang4@sinorail.com>
2024-07-25 10:57:23 +0800

    完成键盘

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -52,7 +52,7 @@ static void init_keymap() {
 
 #define KEY_QUEUE_LEN 1024
 static int key_queue[KEY_QUEUE_LEN] = {};
-static int key_f = 0, key_r = 0;
+static int key_f = 0, key_r = 0;  // 读指针，写指针
 
 static void key_enqueue(uint32_t am_scancode) {
   key_queue[key_r] = am_scancode;
